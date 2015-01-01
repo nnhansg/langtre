8273f31b2291b3ff06e03c81c8672fe8412c4557
Tam Nguyen <nguyentamvinhlong@gmail.com>
2015-01-01 23:38:52 +0700

    add lib active record

---
+++ init_activerecord.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'lib/php-activerecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg) {
+    $cfg->set_model_directory('models');
+    $cfg->set_connections(array(
+        'development' => 'mysql://root:@localhost/ar'
+    ));
+});
